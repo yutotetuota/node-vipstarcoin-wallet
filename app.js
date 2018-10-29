e1bbc170ed128e548f4e0fe9a23499a16c6328b0
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-10-29 17:21:29 +0900

    Fix

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var express = require('express')
   , http = require('http')
   , fs = require('fs')
   , path = require('path')
-  , auth = require('./auth')
+  , auth = require('./auth.js')
   , server_proxy = require('./server_proxy.js');
 
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 9000);
+  app.set('port', process.env.PORT || 8080);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
